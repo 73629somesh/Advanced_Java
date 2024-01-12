57ceb68e27c32c0fe79c4919ab08d15a59b1d548
Somesh Kudal <125436684+73629somesh@users.noreply.github.com>
2024-01-12 16:32:28 +0530

    Single Table Inheritance

---
+++ InheritenceSingleTable/src/main/java/com/demo/model/Product.java
@@ -0,0 +1,69 @@
+package com.demo.model;
+
+
+
+import java.util.Date;
+
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity(name="ProductTab")
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="type",discriminatorType=DiscriminatorType.STRING)
+public class Product {
+@Id
+private int id;
+private String pname;
+private Date mfgdate;
+
+public Product() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+
+public Product(int id, String pname, Date mfgdate) {
+	super();
+	this.id = id;
+	this.pname = pname;
+	this.mfgdate = mfgdate;
+}
+
+public int getId() {
+	return id;
+}
+
+public void setId(int id) {
+	this.id = id;
+}
+
+public String getPname() {
+	return pname;
+}
+
+public void setPname(String pname) {
+	this.pname = pname;
+}
+
+public Date getMfgdate() {
+	return mfgdate;
+}
+
+public void setMfgdate(Date mfgdate) {
+	this.mfgdate = mfgdate;
+}
+
+@Override
+public String toString() {
+	return "Product [id=" + id + ", pname=" + pname + ", mfgdate=" + mfgdate + "]";
+}
+
+
+
+
+	
+	
+}
